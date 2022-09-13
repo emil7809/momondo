1cce360bf9aa63308a7b2380fb6ce0f193b7fddc
emily <emil7809@gmail.com>
2022-09-13 10:11:04 +0200

    search link added

--- comp_flight_form.php
+++ comp_flight_form.php
@@ -17,63 +17,71 @@
         </div>
       </div>
 
-      <form id="flight_form">
-
-        <?php
-        if ($_title == 'Momondo') {
-          echo ' <div id="from_to_container" class="blue_border">';
-        }
-        if ($_title == 'Flight Search') {
-          echo ' <div id="from_to_container">';
-        } ?>
+      <?php
+      if ($_title == 'Momondo') {
+        echo ' <form id="flight_form" action="http://localhost/flight_search">';
+      }
+      if ($_title == 'Flight Search') {
+        echo ' <form id="flight_form"> ';
+      } ?>
 
 
-        <div id="from-container">
-          <div class="input-caontainer">
-            <?= $_airplane ?>
-            <input id="from-input" type="text" placeholder="From?" oninput="show_from_results()">
-          </div>
-          <div id="from-results"></div>
-        </div>
-        <?php
-        if ($_title == 'Momondo') {
-          echo '<div id="switch" class="switch_blue_border">';
-        }
-        if ($_title == 'Flight Search') {
-          echo '<div id="switch">';
-        } ?>
 
-        <?= $_arrows ?>
-        </div>
-        <div id="to-container">
-          <div class="input-caontainer">
-            <?= $_airplane ?>
-            <input id="to-input" type="text" placeholder="To?" oninput="show_to_results()">
-          </div>
-          <div id="to-results"></div>
-        </div>
-        </div>
-        <?php
-        if ($_title == 'Momondo') {
-          echo '<div id="time_container" class="blue_border">';
-        }
-        if ($_title == 'Flight Search') {
-          echo '<div id="time_container">';
-        } ?>
+      <?php
+      if ($_title == 'Momondo') {
+        echo ' <div id="from_to_container" class="blue_border">';
+      }
+      if ($_title == 'Flight Search') {
+        echo ' <div id="from_to_container">';
+      } ?>
 
+
+      <div id="from-container">
         <div class="input-caontainer">
-          <input type="date">
-          <div class="line"></div>
-          <input type="date">
+          <?= $_airplane ?>
+          <input id="from-input" type="text" placeholder="From?" oninput="show_from_results()">
         </div>
+        <div id="from-results"></div>
+      </div>
+      <?php
+      if ($_title == 'Momondo') {
+        echo '<div id="switch" class="switch_blue_border">';
+      }
+      if ($_title == 'Flight Search') {
+        echo '<div id="switch">';
+      } ?>
+
+      <?= $_arrows ?>
+      </div>
+      <div id="to-container">
+        <div class="input-caontainer">
+          <?= $_airplane ?>
+          <input id="to-input" type="text" placeholder="To?" oninput="show_to_results()">
         </div>
-        <?php
-        if ($_title == 'Momondo') {
-          echo '<button type="button" class="button_type_3">Search</button>';
-        }
-        if ($_title == 'Flight Search') {
-          echo '<button type="button">Search</button>';
-        } ?>
+        <div id="to-results"></div>
+      </div>
+      </div>
+      <?php
+      if ($_title == 'Momondo') {
+        echo '<div id="time_container" class="blue_border">';
+      }
+      if ($_title == 'Flight Search') {
+        echo '<div id="time_container">';
+      } ?>
+
+      <div class="input-caontainer">
+        <input type="date">
+        <div class="line"></div>
+        <input type="date">
+      </div>
+      </div>
+      <?php
+      if ($_title == 'Momondo') {
+        echo '<button class="button_type_3">Search</button>';
+      }
+      if ($_title == 'Flight Search') {
+        echo '<button type="button">Search</button>';
+      } ?>
 
 
       </form>
