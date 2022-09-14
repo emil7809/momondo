a559fbae678ed550a8ed76ec1cc121be90a06716
emily <emil7809@gmail.com>
2022-09-14 18:01:53 +0200

    footer responsive

--- comp_footer.php
+++ comp_footer.php
@@ -1,4 +1,75 @@
-<footer>Footer</footer>
+<footer>
+    <div class="center_container">
+        <div id="top_mid_container">
+            <div class="top">
+                <div class="grid">
+                    <h4>Company</h4>
+                    <a href="#">About</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Mobile</a>
+                    <a href="#">Discover</a>
+                    <a href="#">How we work</a>
+                    <a href="#">Momondo coupon codes</a>
+                </div>
+                <div class="grid">
+                    <h4>Contact</h4>
+                    <a href="#">About</a>
+                    <a href="#">Careers</a>
+                    <a href="#">Mobile</a>
+                </div>
+                <div class="grid">
+                    <h4>More</h4>
+                    <a href="#">About</a>
+                    <a href="#">Careers</a>
+                </div>
+            </div>
+            <div class="mid">
+                <h4 style="display: none;">Site / Currency</h4>
+                <div class="border_button">
+                    <div>
+                        <h5>Site</h5>
+                        <div class="flex">
+                            <img id="uk-icon" style="width: 20px; display: none;" src="img/uk-icon.webp" alt="uk icon" />
+                            <p>United Kingdome</p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg style="width:1.2rem;fill:white;margin-left:6px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+                            <path d="M100 135a4.99 4.99 0 0 1-3.363-1.3l-55-50a5 5 0 1 1 6.727-7.399L100 123.243L151.637 76.3a5 5 0 1 1 6.726 7.399l-55 50A4.986 4.986 0 0 1 100 135z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="border_button">
+                    <div>
+                        <h5>Currency</h5>
+                        <div class="flex">
+                            <img id="pund-icon" style="width: 20px;  display: none;" src="img/pound-icon.webp" alt="pound icon" />
+                            <p>Pound Sterling</p>
+                        </div>
+                    </div>
+                    <div>
+                        <svg style="width:1.2rem;fill:white;margin-left:6px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+                            <path d="M100 135a4.99 4.99 0 0 1-3.363-1.3l-55-50a5 5 0 1 1 6.727-7.399L100 123.243L151.637 76.3a5 5 0 1 1 6.726 7.399l-55 50A4.986 4.986 0 0 1 100 135z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bot">
+            <div class="flex">
+                <p>Privacy</p>
+                <p>Terms & Conditions</p>
+                <p>Imprint</p>
+            </div>
+            <div class="flex">
+                <p>©2022 momondo lol not realy its a clone</p>
+            </div>
+        </div>
+        <img id="footer_img" style="display: none;" src="img/footer.webp" alt="footer img">
+    </div>
+</footer>
 
 <script src="app.js"></script>
 
