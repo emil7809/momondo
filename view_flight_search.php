670a91c75da4b7d00581affe96398570c71bd311
emily <emil7809@gmail.com>
2022-09-14 19:05:19 +0200

    form on flight search is responsive

--- view_flight_search.php
+++ view_flight_search.php
@@ -1,19 +1,21 @@
 <?php $_title = 'Flight Search';
 require_once __DIR__ . '/comp_header.php'; ?>
 
-<div id="flights-search">
-  <div class="center_container">
-    <?php require_once __DIR__ . '/comp_flight_form.php'; ?>
+<main id="flight_main">
+  <div id="flights-search">
+    <div class="center_container">
+      <?php require_once __DIR__ . '/comp_flight_form.php'; ?>
+    </div>
   </div>
-</div>
 
-<div id="container">
-  <div id="left">
-    left
+  <div id="container">
+    <div id="left">
+      left
+    </div>
+    <div id="right">
+      right
+    </div>
   </div>
-  <div id="right">
-    right
-  </div>
-</div>
+</main>
 
 <?php require_once __DIR__ . '/comp_footer.php' ?>
